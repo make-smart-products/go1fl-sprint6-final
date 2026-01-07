d57bc2f243dad1228ffdfcb67ed4810f869c2ad6
Dmitriy Efremov <effremovds@yandex.ru>
2026-01-07 23:31:11 +0300

    first commit

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,53 @@
 package handlers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) { // Обработчик запроса к корневому пути ("/")
+	http.ServeFile(w, r, "index.html") // Отправка файла index.html как ответа
+}
+
+func UploadHandler(w http.ResponseWriter, r *http.Request) { // Обработчик загрузки файлов
+	if err := r.ParseMultipartForm(10 << 20); err != nil { // Парсинг формы с ограничением размера 10 МБ
+		http.Error(w, "Ошибка парсинга", http.StatusBadRequest) // Ошибка парсинга — возвращаем 400
+		return
+	}
+
+	file, handler, err := r.FormFile("myFile") // Получение файла из формы по ключу "myFile"
+	if err != nil {
+		http.Error(w, "Ошибка получения файла", http.StatusInternalServerError) // Ошибка получения файла — 500
+		return
+	}
+	defer file.Close() // Закрытие файла по завершении функции
+
+	data, err := io.ReadAll(file) // Чтение всего содержимого файла в память
+	if err != nil {
+		http.Error(w, "Ошибка чтения файла", http.StatusBadRequest) // Ошибка чтения — 400
+		return
+	}
+
+	result := service.Converter(string(data)) // Преобразование содержимого через внешнюю функцию Converter
+
+	ext := filepath.Ext(handler.Filename)                                             // Получение расширения исходного файла
+	filename := time.Now().UTC().Add(3*time.Hour).Format("07.01.2026 13-15-05") + ext // Создание имени файла с текущей датой и временем + расширение
+	out, err := os.Create(filename)                                                   // Создание нового файла для записи результата
+	if err != nil {
+		http.Error(w, "Ошибка создания файла", http.StatusInternalServerError) // Ошибка создания файла — 500
+		return
+	}
+	defer out.Close() // Закрытие файла по окончании функции
+
+	if _, err := out.WriteString(result); err != nil { // Запись строки результата в созданный файл
+		http.Error(w, "Ошибка записи файла", http.StatusInternalServerError) // Ошибка записи — 500
+		return
+	}
+
+	w.Write([]byte(result)) // Отправка результата конвертации клиенту в HTTP-ответе
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,16 @@
 package service
+
+import (
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Converter(input string) string { // Функция Converter принимает строку и возвращает строку
+
+	for _, char := range input { // Перебор каждого символа во входной строке
+		if char != '.' && char != '-' && char != ' ' {
+			return morse.ToMorse(input) // Конвертируем всю строку из обычного текста в азбуку Морзе
+		}
+	}
+
+	return morse.ToText(input) // Если все символы были из множества "-. ", конвертируем из Морзе в текст
+}
